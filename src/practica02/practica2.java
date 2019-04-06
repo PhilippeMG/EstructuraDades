d7bb77570c35afc7add67e9b3740a8fb55e90b3d
al375923 <al375923@uji.es>
2019-04-06 20:57:59 +0200

    Segona activitat superada, invierte pasa el test

--- src/practica02/practica2.java
+++ src/practica02/practica2.java
@@ -4,43 +4,59 @@ import java.util.*;
 
 public class practica2 {
 
-    /** Comprueba si dos listas de String son equivalentes.
-     *
+    /**
+     * Comprueba si dos listas de String son equivalentes.
+     * <p>
      * Dos listas son equivalentes si contienen los mismos elementos y la misma cantidad de ellos.
-     * @param l1    Primera lista
-     * @param l2    Segunda lista
+     *
+     * @param l1 Primera lista
+     * @param l2 Segunda lista
      * @return <code>true</code> si las listas son equivalentes. <code>false</code> en caso contrario.
      */
     static public boolean equivalentes(List<String> l1, List<String> l2) {
-       //TODO completa la implmentación
-        int posicio=0;
-        List<String> aux=new LinkedList<>();
+        //TODO completa la implmentación
+        int posicio = 0;
+        List<String> aux = new LinkedList<>();
         aux.addAll(l2);
-        if(l1.size()==aux.size()) {
+        if (l1.size() == aux.size()) {
             for (String element : l1) {
-                 posicio = aux.indexOf(element);
+                posicio = aux.indexOf(element);
                 if (posicio < 0) return false;
                 aux.remove(posicio);
             }
-            if (aux.size()==0) return true;
+            if (aux.size() == 0) return true;
         }
         return false;
 
 
     }
 
-    /** Invierte el orden de los elmentos de una lista.
+    /**
+     * Invierte el orden de los elmentos de una lista.
      *
      * @param iter Un iterador de la lista. Puede estar en cualqueir posición de la lista.
      */
     static public void invierte(ListIterator<String> iter) {
         //TODO completa la implmentación
+        while (iter.hasNext()) iter.next();
+        List<String> aux = new LinkedList<>();
+        while (iter.hasPrevious()) {
+            aux.add(iter.previous());
+        }
+        System.out.printf(aux.toString());
+        for (String element : aux) {
+            iter.next();
+            iter.set(element);
+        }
+
     }
 
 
-    /** Ordena los elementos de una lista de menor a mayor
-     * @param l     La lista
-     * @return      Una nueva lista con los mismo elementos, pero ordenados.
+    /**
+     * Ordena los elementos de una lista de menor a mayor
+     *
+     * @param l La lista
+     * @return Una nueva lista con los mismo elementos, pero ordenados.
      */
     static public List<Integer> ordenar(List<Integer> l) {
         //TODO completa la implmentación
